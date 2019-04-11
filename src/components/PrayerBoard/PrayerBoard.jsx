2811783b6df6f65568042076609918724add3e91
annaelizabeth2019 <annapeterson89@gmail.com>
2019-04-11 09:00:12 -0500

    morning commit

--- src/components/PrayerBoard/PrayerBoard.jsx
+++ src/components/PrayerBoard/PrayerBoard.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-import styles from './PrayerBoard.module.css'
-import Prayer from '../Prayer/Prayer'
+import styles from './PrayerBoard.css'
+import PrayerCard from '../PrayerCard/PrayerCard'
 import PrayerService from '../../utils/prayersService'
 import loading from '../../images/loading.gif'
 
@@ -16,21 +16,23 @@ class PrayerBoard extends Component {
         }
         render() {
             return (
-                <div className={styles.PrayerBoard}>
-                    {this.state.prayers.length > 1 ? this.state.prayers.map((prayer, idx) => 
-                        <Prayer 
-                        key={idx}
-                        text={prayer.text}
-                        location={prayer.location}
-                        higherPower={prayer.higherPower}
-                        /> 
-                    ) : 
-                    <div className="noPrayers flex-column center">
-                        <code>Prayers ar Being Fetched From the Cloud. Please Wait.</code>
-                        <br /><img src={loading} alt="Blocks boucing around" /><br />
-                        <code>if you are not automatically redirected please consult your nearest <br />s p i r i t u a l &nbsp; l e a d e r .</code>
+                <div className="PrayerBoard">
+                    <div classname="card blue">
+                        {this.state.prayers.length > 1 ? this.state.prayers.map((prayer, idx) => 
+                            <PrayerCard
+                                key={idx}
+                                text={prayer.text}
+                                location={prayer.location}
+                                higherPower={prayer.higherPower}
+                                /> 
+                        ) : 
+                        <div className="noPrayers flex-column center">
+                            <code>Prayers ar Being Fetched From the Cloud. Please Wait.</code>
+                            <br /><img src={loading} alt="Blocks boucing around" /><br />
+                            <code>if you are not automatically redirected please consult your nearest <br />s p i r i t u a l &nbsp; l e a d e r .</code>
+                        </div>
+                        }    
                     </div>
-                    }    
                 </div>
             );
         }
---
+++ src/components/PrayerCard/PrayerCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './PrayerCard.css'
+
+const PrayerCard = (props) => (
+    <div className={styles.PrayerCard}>
+        <div className="card black-text PrayerCard">
+        <div class="card-content">
+            <span class="card-title">{props.text}</span>
+        </div>
+        </div>
+    </div>
+);
+
+export default PrayerCard;
