1ee3b20794bf51b766bb9ae89efb1fc93393553a
annaelizabeth2019 <annapeterson89@gmail.com>
2019-04-15 22:57:07 -0500

    paranoid about whether I pushed latest changes

--- src/components/PrayerCard/PrayerCard.jsx
+++ src/components/PrayerCard/PrayerCard.jsx
@@ -6,10 +6,10 @@ const PrayerCard = (props) => (
         <div className="card-image waves-effect waves-block waves-light ema"><img src="https://icon-rainbow.com/i/icon_04219/icon_042190_256.png" alt="a logo" className="activator img-small " />
         </div>
         <div className="card-content center">
-            <span className="card-title activator grey-text text-darken-4 truncate">{props.text}<i className="material-icons right">arrow_drop_up</i></span>
+            <span className="card-title activator grey-text text-darken-4 truncate">{props.text}<i className="material-icons right">note</i></span>
         </div>
         <div className="card-reveal black white-text">
-            <span className="card-title">{props.text}<i className="material-icons right">close</i></span>
+            <span className="card-title">{props.text}<i className="material-icons right">note</i></span>
             <table>
                 <tbody>
                     <th className="center">This prayer was sent to:</th>
