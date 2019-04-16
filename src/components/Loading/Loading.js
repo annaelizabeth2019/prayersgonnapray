bdaf3df3e23cde3ade4b389e5711f1dc422f5448
annaelizabeth2019 <annapeterson89@gmail.com>
2019-04-15 23:20:46 -0500

    fixed a typo

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -5,7 +5,7 @@ import loading from '../../images/loading.gif'
 const Loading = (props) =>{
     return (
         <div className="noPrayers flex-column center">
-            <code>Prayers ar Being Fetched From the Cloud. Please Wait or try praying.</code>
+            <code>Prayers are Being Fetched From the Cloud. Please Wait or try &nbsp; p r a y i n g.</code>
             <br /><img src={loading} alt="Blocks boucing around" className="img-small" /><br />
             <code>if nothing comes to you, contact your nearest <br />s p i r i t u a l &nbsp; l e a d e r .</code>
         </div>
